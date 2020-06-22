aaac457f50cc03c5d3eabbb846798cbe7eed5850
tashfeenrao <raotashfeen25@gmail.com>
2020-06-23 03:13:52 +0500

    added flex dirextion to group and button panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,31 +8,31 @@ class ButtonPanel extends Component {
     const { onClick } = this.props;
     return (
       <div className="buttonPanel">
-        <div className="Group1">
+        <div className="Group">
           <Button name="Button" value="AC" onClick={onClick} />
           <Button name="Button" value="+/-" onClick={onClick} />
           <Button name="Button" value="%" onClick={onClick} />
           <Button name="Button" value="÷" onClick={onClick} />
         </div>
-        <div className="Group2">
+        <div className="Group">
           <Button name="Button" value="7" onClick={onClick} />
           <Button name="Button" value="8" onClick={onClick} />
           <Button name="Button" value="9" onClick={onClick} />
           <Button name="Button" value="X" onClick={onClick} />
         </div>
-        <div className="Group3">
+        <div className="Group">
           <Button name="Button" value="4" onClick={onClick} />
           <Button name="Button" value="5" onClick={onClick} />
           <Button name="Button" value="6" onClick={onClick} />
           <Button name="Button" value="-" onClick={onClick} />
         </div>
-        <div className="Group4">
+        <div className="Group">
           <Button name="Button" value="1" onClick={onClick} />
           <Button name="Button" value="2" onClick={onClick} />
           <Button name="Button" value="3" onClick={onClick} />
           <Button name="Button" value="+" onClick={onClick} />
         </div>
-        <div className="Group5">
+        <div className="Group">
           <Button name="Button" value="0" onClick={onClick} />
           <Button name="Button" value="." onClick={onClick} />
           <Button name="Button" value="=" onClick={onClick} />

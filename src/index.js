6d0501c31cfca83319efb8d0c1cc96b3456fb8c0
tashfeenrao <raotashfeen25@gmail.com>
2020-06-23 04:11:48 +0500

    added module.hot for debugging added height to group div

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+if(module.hot) {
+  module.hot.accept();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

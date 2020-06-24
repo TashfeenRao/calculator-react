1765036650d5a5505a9c7faf6da54a13292cdc4a
tashfeenrao <raotashfeen25@gmail.com>
2020-06-25 00:57:50 +0500

    calculate module implemented

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,4 +1,40 @@
+import operate from './operate';
+
 const calculate = (data, btnName) => {
- 
-}
-export default calculate
+  const imData = data;
+  switch (btnName) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      imData.next += btnName;
+      break;
+    case '+/-':
+      imData.total *= -1;
+      imData.next *= -1;
+      break;
+    case 'AC':
+      imData.total = 0;
+      imData.next = 0;
+      imData.operation = null;
+      break;
+    case '%':
+      if (imData.total && parseFloat(imData.total) > 0) {
+        imData.total = operate(imData.total, 100, '÷');
+      }
+      if (imData.next && parseFloat(imData.next) > 0) {
+        imData.next = operate(imData.next, 100, '÷');
+      }
+      break;
+    default:
+      imData.total = operate(...data);
+      imData.next = 0;
+      imData.operation = null;
+  }
+};
+export default calculate;
